ce5d45b2ddd6c1d32003e04b71cc6e2deefa151e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-03-30 05:31:10 -0300

    Saving files in the files subfolder, putting relative paths and adding output descriptions in the folder 02

--- 02_data_transformation/main.py
+++ 02_data_transformation/main.py
@@ -2,14 +2,18 @@ import os
 import tabula
 import pandas as pd
 import zipfile
-from io import BytesIO
 
-pdf_path = "../01_web_scraping/pdfs/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
-csv_path = "tabela_extraida.csv"
-zip_name = "Teste_Rubens_Rabelo.zip"
+output_dir = "files"
+os.makedirs(output_dir, exist_ok=True)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+pdf_path = os.path.join(script_dir, "..", "01_web_scraping", "files", "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")
+csv_path = os.path.join(output_dir, "tabela_extraida.csv")
+zip_name = os.path.join(output_dir, "Teste_Rubens_Rabelo.zip")
 
 
 def extract_table_from_pdf(pdf_path):
+    print("Transformando a tabela do Anexo 1 em DataFrame")
     tables = tabula.read_pdf(pdf_path, pages="3-181", multiple_tables=True)
     filtered_tables = [table for table in tables if table.shape[1] == 13]
     for i, table in enumerate(filtered_tables):
@@ -27,14 +31,17 @@ def rename_columns(df):
 
 
 def save_csv(df, csv_path):
+    print("Gerando o CSV")
     df.to_csv(csv_path, index=False, encoding="utf-8")
     print(f"CSV salvo em {csv_path}")
 
 
 def compress_csv(csv_path, zip_name):
+    print("Compactando o CSV em formato ZIP")
     with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zipf:
         zipf.write(csv_path, os.path.basename(csv_path))
     print(f"Arquivo compactado como {zip_name}")
+
 
 df = extract_table_from_pdf(pdf_path)
 df = rename_columns(df)
